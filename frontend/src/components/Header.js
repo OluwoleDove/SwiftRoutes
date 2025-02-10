a99464d2a2f7e4e0e59d4d66ac5cbb834ca1be0b
OluwoleDove <ladmans07@gmail.com>
2025-02-10 07:01:50 +0100

    Updating repo and Preparing for the day's work

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,6 +12,7 @@ const Header = () => {
           <li><Link to="/planner">Planner</Link></li>
           <li><Link to="/rewards">Rewards</Link></li>
           <li><Link to="/login">Login</Link></li>
+          <li><Link to="/sudoku">Sudoku</Link></li>
         </ul>
       </nav>
     </header>
